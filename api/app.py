e726275f381d3d2acbfbb5a294265409fbada4a1
Vincent Wang <wangosky@gmail.com>
2023-08-29 16:53:50 +0800

    fix: 修复登录冲突

--- api/app.py
+++ api/app.py
@@ -236,5 +236,5 @@ def threads():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001, debug=True)
-    # app.run(host='0.0.0.0', port=5001)
+    # app.run(host='0.0.0.0', port=5001, debug=True)
+    app.run(host='0.0.0.0', port=5001)
